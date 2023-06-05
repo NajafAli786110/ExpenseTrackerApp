0c3e3dcda6f9b86e2a0611d56e4d66df6e4a12f1
Najaf Ali <najaf.balti000@gmail.com>
2023-06-05 17:09:46 +0500

    First Commit

---
+++ expense-tracker/src/Components/Expenses/Expenses.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expenseContent=<p className="No-found">No Expenses Found</p>;
+
+  if(filteredExpenses.length>0){
+    expenseContent=filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenseContent}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
